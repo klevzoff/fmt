436823fa764437b9f8df14c046d26afe1a5862cb
Sergey Klevtsov <klevtsov@stanford.edu>
2021-11-12 21:39:21 -0800

    Work around nvcc unrecognized pragma warning

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -280,7 +280,7 @@
 
 // Enable minimal optimizations for more compact code in debug mode.
 FMT_GCC_PRAGMA("GCC push_options")
-#ifndef __OPTIMIZE__
+#if !defined(__OPTIMIZE__) && !FMT_NVCC
 FMT_GCC_PRAGMA("GCC optimize(\"Og\")")
 #endif
 
